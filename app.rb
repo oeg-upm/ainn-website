33229c85bc63430edf68d393836488d64dc540d3
Ahmad Alobaid <ahmad88me@gmail.com>
2018-07-26 18:46:00 +0200

    add semantic-ui styles ,register, and msg

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require "sinatra"
 
+get "/register" do
+    erb :register
+end
+
+post "/register" do
+    "Registered"
+    erb :msg, :locals => {:msg => "Registered"}
+end
+
 get "/" do
     erb :home
 end
-
