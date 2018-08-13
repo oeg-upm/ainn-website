8e7d11d1a23af8f70d42d4123326b16815498e4f
Ahmad Alobaid <ahmad88me@gmail.com>
2018-08-13 17:17:30 +0200

    update startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 export WORKSPACE=/home/vagrant
-nohup sh $WORKSPACE/ainn-userm/run.sh &
-nohup $WORKSPACE/ainn-request/.venv/bin/python app.py &
-cd $WORKSPACE/mappingpedia-engine/mappingpedia-engine-datasets-ws;nohup mvn clean spring-boot:run &
+sudo mongod --config /etc/mongod.conf &
+cd $WORKSPACE/ainn-userm; go build; ./ainn-userm
+$WORKSPACE/ainn-request/.venv/bin/python app.py &
+cd $WORKSPACE/mappingpedia-engine/mappingpedia-engine-datasets-ws; mvn clean spring-boot:run &
 cd $WORKSPACE/ainn-website
