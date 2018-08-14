ac1b6e69456656f919c5fff4a936e6082750cce8
Ahmad Alobaid <ahmad88me@gmail.com>
2018-08-14 15:12:38 +0200

    update startup

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@ export WORKSPACE=/home/vagrant
 sudo mongod --config /etc/mongod.conf &
 export GOROOT=/usr/lib/go-1.10
 export PATH=$PATH:$GOROOT/bin
+sudo chown $USER -R $WORKSPACE
 cd $WORKSPACE/ainn-userm; go build; ./ainn-userm &
 $WORKSPACE/ainn-request/.venv/bin/python app.py &
 cd $WORKSPACE/mappingpedia-engine/mappingpedia-engine-datasets-ws; mvn clean spring-boot:run &
-cd $WORKSPACE/ainn-website
