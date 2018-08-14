b98716485d5eb0649a7d5cc2616d19aa53c1204b
fpriyatna <freddy.priyatna@gmail.com>
2018-08-14 12:10:45 +0200

    update startup

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 export WORKSPACE=/home/vagrant
+export GOROOT=/usr/lib/go-1.10
+export PATH=$PATH:$GOROOT/bin
 sudo mongod --config /etc/mongod.conf &
 cd $WORKSPACE/ainn-userm; go build; ./ainn-userm
 $WORKSPACE/ainn-request/.venv/bin/python app.py &
