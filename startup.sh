010b723a2fbf7408b1d166af7c240033f057f666
Freddy Priyatna <fpriyatna@fi.upm.es>
2018-08-14 15:34:39 +0000

    install mappingpedia commons and datasets in startup

--- startup.sh
+++ startup.sh
@@ -5,4 +5,6 @@ export PATH=$PATH:$GOROOT/bin
 sudo chown $USER -R $WORKSPACE
 cd $WORKSPACE/ainn-userm; go build; ./ainn-userm &
 cd $WORKSPACE/ainn-request/; .venv/bin/python app.py &
+cd $WORKSPACE/mappingpedia-engine/mappingpedia-engine-commons; mvn clean install
+cd $WORKSPACE/mappingpedia-engine/mappingpedia-engine-datasets; mvn clean install
 cd $WORKSPACE/mappingpedia-engine/mappingpedia-engine-datasets-ws; mvn clean spring-boot:run &
