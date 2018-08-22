3cce7ad2dea06f94952951fa6185db5ac8914cfa
Freddy Priyatna <freddy.priyatna@gmail.com>
2018-08-22 12:37:22 +0200

    update bootstrap-sample

--- bootstrap-sample.sh
+++ bootstrap-sample.sh
@@ -1,78 +1,82 @@
-export WORKSPACE=/home/vagrant
-# Requests
-echo "Setting up requests micro-service ..."
+export WORKSPACE=$HOME
+export USERHOME=$HOME
+## Requests
+clear
+echo "SETTING UP REQUESTS MICRO-SERVICE ..."
 sudo apt-get update
 sudo apt-get install python-pip python-dev build-essential -y
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 cd $WORKSPACE; git clone https://github.com/oeg-upm/ainn-request.git
 sudo pip install virtualenv
-cd $WORKSPACE/ainn-request; virtualenv -p /usr/bin/python2.7 .venv
+cd $WORKSPACE/ainn-request
+git pull
+virtualenv -p /usr/bin/python2.7 .venv
 $WORKSPACE/ainn-request/.venv/bin/pip install -r requirements.txt
-nohup $WORKSPACE/ainn-request/.venv/bin/python app.py &
 
 # User management
-echo "Setting up mongoDB ..."
+clear
+echo "Setting up MongoDB ..."
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo service mongod restart
-echo "Setting up user-management micro-service"
+echo "SETTING UP USER MANAGEMENT MICRO SERVICE"
 sudo add-apt-repository ppa:gophers/archive -y
 sudo apt-get update
 sudo apt-get install golang-1.10-go -y
 export GOROOT=/usr/lib/go-1.10
 export PATH=$PATH:$GOROOT/bin
-go get github.com/gorilla/mux
-go get github.com/mongodb/mongo-go-driver/bson
+cd $WORKSPACE; mkdir go;
+export GOPATH=$WORKSPACE/go
+echo "Installing libraries for GO"
 cd $WORKSPACE;git clone https://github.com/oeg-upm/ainn-userm.git
-cd $WORKSPACE/ainn-userm; go build; nohup ./ainn-userm &
+cd $WORKSPACE/ainn-userm;go get github.com/gorilla/mux
+cd $WORKSPACE/ainn-userm;go get github.com/mongodb/mongo-go-driver/bson
+
 
 #commands to install mappingpedia-engine-datasets
-echo "Installing Mappingpedia ..."
+clear
+echo "SETTING UP MAPPINGPEDIA-ENGINE ..."
+cd $WORKSPACE
 sudo apt-get update
 sudo apt-get install maven -y
-sudo apt-get install openjdk-8-jdk-headless
+sudo apt-get install openjdk-8-jdk-headless -y
 mkdir mappingpedia-engine
 cd mappingpedia-engine
 git clone https://github.com/oeg-upm/mappingpedia-engine-commons
 git clone https://github.com/oeg-upm/mappingpedia-engine-datasets
 git clone https://github.com/oeg-upm/mappingpedia-engine-datasets-ws
 cd mappingpedia-engine-commons
+git pull
 touch src/main/resources/config.properties
 echo virtuoso.enabled=true >> src/main/resources/config.properties
 echo cleargraph=false >> src/main/resources/config.properties
 echo vjdbc=jdbc:virtuoso://localhost:1111 >> src/main/resources/config.properties
-echo usr= >> src/main/resources/config.properties
-echo pwd= >> src/main/resources/config.properties
-echo graphname= >> src/main/resources/config.properties
+echo usr=REPLACEME >> src/main/resources/config.properties
+echo pwd=REPLACEME >> src/main/resources/config.properties
+echo graphname=http://mappingpedia.linkeddata.es/graph/datasets >> src/main/resources/config.properties
 echo github.enabled=true >> src/main/resources/config.properties
-echo github.mappingpedia.username= >> src/main/resources/config.properties
-echo github.mappingpedia.accesstoken= >> src/main/resources/config.properties
-echo github.repository= >> src/main/resources/config.properties
+echo github.mappingpedia.username=REPLACEME >> src/main/resources/config.properties
+echo github.mappingpedia.accesstoken=REPLACEME >> src/main/resources/config.properties
+echo github.repository=REPLACEME >> src/main/resources/config.properties
 echo ckan.enabled=true >> src/main/resources/config.properties
-echo ckan.url= >> src/main/resources/config.properties
-echo ckan.key= >> src/main/resources/config.properties
-mvn clean install
-cd ..
-cd mappingpedia-engine-datasets
-mvn clean install
-cd ..
-cd mappingpedia-engine-datasets-ws
-nohup mvn clean spring-boot:run &
+echo ckan.url=http://localhost/ckan >> src/main/resources/config.properties
+echo ckan.key=REPLACEME >> src/main/resources/config.properties
 
 # Website
-echo "Installing the Website ..."
-#sudo apt-get install ruby -y
+clear
+echo "SETTING UP AI.NNOTATION WEBSITE ..."
+sudo apt-get install ruby -y
 cd $WORKSPACE; git clone https://github.com/oeg-upm/ainn-website.git
 sudo apt-get install software-properties-common
 sudo apt-add-repository -y ppa:rael-gc/rvm
 sudo apt-get update
 sudo apt-get install rvm -y
 echo "source /etc/profile.d/rvm.sh" >> ~/.bash_profile
-cd $WORKSPACE/ainn-website;
+cd $WORKSPACE/ainn-website
+git pull
 /etc/profile.d/rvm.sh install ruby
-#rvm install ruby
 sudo gem install sinatra
 sudo gem install sinatra-reloader
 sudo gem install passenger
@@ -80,7 +84,9 @@ sudo gem install passenger
 sudo apt-get install libcurl4-openssl-dev ruby-dev libssl-dev apache2-dev libapr1-dev libaprutil1-dev -y
 
 # Set up Apache
+clear
 echo "Setting up apache"
+sudo apt-get install apache2 -y
 cat <<EOT >> /etc/apache2/sites-available/ainnwebsite.conf
 <VirtualHost *:80>
 ServerAdmin ahmad.alobaid@accenture.com
@@ -94,37 +100,5 @@ ErrorLog ${APACHE_LOG_DIR}/error.log
 CustomLog ${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
 EOT
-
 sudo a2dissite 000-default.conf
 sudo a2ensite ainnwebsite.conf
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
