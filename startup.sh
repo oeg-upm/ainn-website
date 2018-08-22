8f7eaaea9284057f035077ca283d66dcbfe314c6
Freddy Priyatna <freddy.priyatna@gmail.com>
2018-08-22 11:32:42 +0200

    add git pull command before installing/running mappingpedia

--- startup.sh
+++ startup.sh
@@ -1,10 +1,17 @@
-export WORKSPACE=/home/vagrant
-sudo mongod --config /etc/mongod.conf &
+export WORKSPACE=$HOME
 export GOROOT=/usr/lib/go-1.10
 export PATH=$PATH:$GOROOT/bin
+
+sudo mongod --config /etc/mongod.conf &
 sudo chown $USER -R $WORKSPACE
 cd $WORKSPACE/ainn-userm; go build; ./ainn-userm &
 cd $WORKSPACE/ainn-request/; .venv/bin/python app.py &
-cd $WORKSPACE/mappingpedia-engine/mappingpedia-engine-commons; mvn clean install
-cd $WORKSPACE/mappingpedia-engine/mappingpedia-engine-datasets; mvn clean install
-cd $WORKSPACE/mappingpedia-engine/mappingpedia-engine-datasets-ws; mvn clean spring-boot:run &
+cd $WORKSPACE/mappingpedia-engine/mappingpedia-engine-commons
+git pull
+mvn clean install
+cd $WORKSPACE/mappingpedia-engine/mappingpedia-engine-datasets
+git pull
+mvn clean install
+cd $WORKSPACE/mappingpedia-engine/mappingpedia-engine-datasets-ws
+git pull
+mvn clean spring-boot:run &
